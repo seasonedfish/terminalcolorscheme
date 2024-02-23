d4efc26c15684123927dbfdb7b0a5f7e30b0c470
Fisher Sun <f.sun@wustl.edu>
2024-02-23 17:15:24 -0600

    Wrap error when color fails to be parsed

--- src/TerminalTheme.ts
+++ src/TerminalTheme.ts
@@ -29,6 +29,20 @@ class Ansi implements Ansi {
 
 type ColorTranslatorInput = ConstructorParameters<typeof ColorTranslator>;
 
+/**
+ * Creates a ColorTranslator object from an input, or throw an error if this fails.
+ * @param input the input to the ColorTranslator
+ */
+function parseColorOrThrow(...input: ColorTranslatorInput): ColorTranslator {
+    // ColorTranslator throws an error if it fails, but since it is minified, it is hard to read.
+    // Throw our own error.
+    try {
+        return new ColorTranslator(...input);
+    } catch (err) {
+        throw new Error(`Parsing color failed: ${err}`)
+    }
+}
+
 // Builder pattern
 // https://stackoverflow.com/a/53982272/
 class AnsiBuilder implements Partial<Ansi> {
@@ -51,67 +65,67 @@ class AnsiBuilder implements Partial<Ansi> {
     brightWhite?: ColorTranslator;
 
     withBlack(...black: ColorTranslatorInput): this & Pick<Ansi, "black"> {
-        return Object.assign(this, {black: new ColorTranslator(...black)})
+        return Object.assign(this, {black: parseColorOrThrow(...black)})
     }
 
     withRed(...red: ColorTranslatorInput): this & Pick<Ansi, "red"> {
-        return Object.assign(this, { red: new ColorTranslator(...red) });
+        return Object.assign(this, { red: parseColorOrThrow(...red) });
     }
 
     withGreen(...green: ColorTranslatorInput): this & Pick<Ansi, "green"> {
-        return Object.assign(this, { green: new ColorTranslator(...green) });
+        return Object.assign(this, { green: parseColorOrThrow(...green) });
     }
 
     withYellow(...yellow: ColorTranslatorInput): this & Pick<Ansi, "yellow"> {
-        return Object.assign(this, { yellow: new ColorTranslator(...yellow) });
+        return Object.assign(this, { yellow: parseColorOrThrow(...yellow) });
     }    
 
     withBlue(...blue: ColorTranslatorInput): this & Pick<Ansi, "blue"> {
-        return Object.assign(this, { blue: new ColorTranslator(...blue) });
+        return Object.assign(this, { blue: parseColorOrThrow(...blue) });
     }
 
     withMagenta(...magenta: ColorTranslatorInput): this & Pick<Ansi, "magenta"> {
-        return Object.assign(this, { magenta: new ColorTranslator(...magenta) });
+        return Object.assign(this, { magenta: parseColorOrThrow(...magenta) });
     }
 
     withCyan(...cyan: ColorTranslatorInput): this & Pick<Ansi, "cyan"> {
-        return Object.assign(this, { cyan: new ColorTranslator(...cyan) });
+        return Object.assign(this, { cyan: parseColorOrThrow(...cyan) });
     }
 
     withWhite(...white: ColorTranslatorInput): this & Pick<Ansi, "white"> {
-        return Object.assign(this, { white: new ColorTranslator(...white) });
+        return Object.assign(this, { white: parseColorOrThrow(...white) });
     }
 
     withBrightBlack(...brightBlack: ColorTranslatorInput): this & Pick<Ansi, "brightBlack"> {
-        return Object.assign(this, { brightBlack: new ColorTranslator(...brightBlack) });
+        return Object.assign(this, { brightBlack: parseColorOrThrow(...brightBlack) });
     }
 
     withBrightRed(...brightRed: ColorTranslatorInput): this & Pick<Ansi, "brightRed"> {
-        return Object.assign(this, { brightRed: new ColorTranslator(...brightRed) });
+        return Object.assign(this, { brightRed: parseColorOrThrow(...brightRed) });
     }
 
     withBrightGreen(...brightGreen: ColorTranslatorInput): this & Pick<Ansi, "brightGreen"> {
-        return Object.assign(this, { brightGreen: new ColorTranslator(...brightGreen) });
+        return Object.assign(this, { brightGreen: parseColorOrThrow(...brightGreen) });
     }
 
     withBrightYellow(...brightYellow: ColorTranslatorInput): this & Pick<Ansi, "brightYellow"> {
-        return Object.assign(this, { brightYellow: new ColorTranslator(...brightYellow) });
+        return Object.assign(this, { brightYellow: parseColorOrThrow(...brightYellow) });
     }
 
     withBrightBlue(...brightBlue: ColorTranslatorInput): this & Pick<Ansi, "brightBlue"> {
-        return Object.assign(this, { brightBlue: new ColorTranslator(...brightBlue) });
+        return Object.assign(this, { brightBlue: parseColorOrThrow(...brightBlue) });
     }
 
     withBrightMagenta(...brightMagenta: ColorTranslatorInput): this & Pick<Ansi, "brightMagenta"> {
-        return Object.assign(this, { brightMagenta: new ColorTranslator(...brightMagenta) });
+        return Object.assign(this, { brightMagenta: parseColorOrThrow(...brightMagenta) });
     }
 
     withBrightCyan(...brightCyan: ColorTranslatorInput): this & Pick<Ansi, "brightCyan"> {
-        return Object.assign(this, { brightCyan: new ColorTranslator(...brightCyan) });
+        return Object.assign(this, { brightCyan: parseColorOrThrow(...brightCyan) });
     }
 
     withBrightWhite(...brightWhite: ColorTranslatorInput): this & Pick<Ansi, "brightWhite"> {
-        return Object.assign(this, { brightWhite: new ColorTranslator(...brightWhite) });
+        return Object.assign(this, { brightWhite: parseColorOrThrow(...brightWhite) });
     }
 
     build(this: Ansi) {
@@ -190,11 +204,11 @@ class TerminalThemeBuilder implements Partial<TerminalTheme> {
     selectionForeground?: ColorTranslator;
 
     withBackground(...background: ColorTranslatorInput): this & Pick<TerminalTheme, "background"> {
-        return Object.assign(this, { background: new ColorTranslator(...background) });
+        return Object.assign(this, { background: parseColorOrThrow(...background) });
     }
 
     withForeground(...foreground: ColorTranslatorInput): this & Pick<TerminalTheme, "foreground"> {
-        return Object.assign(this, { foreground: new ColorTranslator(...foreground) });
+        return Object.assign(this, { foreground: parseColorOrThrow(...foreground) });
     }
 
     withAnsi(ansi: Ansi): this & Pick<TerminalTheme, "ansi"> {
@@ -202,23 +216,23 @@ class TerminalThemeBuilder implements Partial<TerminalTheme> {
     }
 
     withCursorBackground(...cursorBackground: ColorTranslatorInput): this & Required<Pick<TerminalTheme, "cursorBackground">> {
-        return Object.assign(this, { cursorBackground: new ColorTranslator(...cursorBackground) });
+        return Object.assign(this, { cursorBackground: parseColorOrThrow(...cursorBackground) });
     }
 
     withCursorBorder(...cursorBorder: ColorTranslatorInput): this & Required<Pick<TerminalTheme, "cursorBorder">> {
-        return Object.assign(this, { cursorBorder: new ColorTranslator(...cursorBorder) });
+        return Object.assign(this, { cursorBorder: parseColorOrThrow(...cursorBorder) });
     }
 
     withCursorForeground(...cursorForeground: ColorTranslatorInput): this & Required<Pick<TerminalTheme, "cursorForeground">> {
-        return Object.assign(this, { cursorForeground: new ColorTranslator(...cursorForeground) });
+        return Object.assign(this, { cursorForeground: parseColorOrThrow(...cursorForeground) });
     }
 
     withSelectionBackground(...selectionBackground: ColorTranslatorInput): this & Required<Pick<TerminalTheme, "selectionBackground">> {
-        return Object.assign(this, { selectionBackground: new ColorTranslator(...selectionBackground) });
+        return Object.assign(this, { selectionBackground: parseColorOrThrow(...selectionBackground) });
     }
 
     withSelectionForeground(...selectionForeground: ColorTranslatorInput): this & Required<Pick<TerminalTheme, "selectionForeground">> {
-        return Object.assign(this, { selectionForeground: new ColorTranslator(...selectionForeground) });
+        return Object.assign(this, { selectionForeground: parseColorOrThrow(...selectionForeground) });
     }
 
     build(this: TerminalTheme) {
