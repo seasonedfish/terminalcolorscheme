6f8be20a2b66b4ce0c949f262cc17c025321f502
Fisher Sun <f.sun@wustl.edu>
2024-02-23 10:18:22 -0600

    Take in input to ColorTranslators rather than ColorTranslators

--- src/TermTheme.ts
+++ src/TermTheme.ts
@@ -27,6 +27,8 @@ class Ansi implements Ansi {
     }
 }
 
+type ColorTranslatorInput = ConstructorParameters<typeof ColorTranslator>;
+
 // Builder pattern
 // https://stackoverflow.com/a/53982272/
 class AnsiBuilder implements Partial<Ansi> {
@@ -48,68 +50,68 @@ class AnsiBuilder implements Partial<Ansi> {
     brightCyan?: ColorTranslator;
     brightWhite?: ColorTranslator;
 
-    withBlack(black: ColorTranslator): this & Pick<Ansi, "black"> {
-        return Object.assign(this, {black: black})
+    withBlack(black: ColorTranslatorInput): this & Pick<Ansi, "black"> {
+        return Object.assign(this, {black: new ColorTranslator(...black)})
     }
 
-    withRed(red: ColorTranslator): this & Pick<Ansi, "red"> {
-        return Object.assign(this, { red });
+    withRed(red: ColorTranslatorInput): this & Pick<Ansi, "red"> {
+        return Object.assign(this, { red: new ColorTranslator(...red) });
     }
 
-    withGreen(green: ColorTranslator): this & Pick<Ansi, "green"> {
-        return Object.assign(this, { green });
+    withGreen(green: ColorTranslatorInput): this & Pick<Ansi, "green"> {
+        return Object.assign(this, { green: new ColorTranslator(...green) });
     }
 
-    withYellow(yellow: ColorTranslator): this & Pick<Ansi, "yellow"> {
-        return Object.assign(this, { yellow });
+    withYellow(yellow: ColorTranslatorInput): this & Pick<Ansi, "yellow"> {
+        return Object.assign(this, { yellow: new ColorTranslator(...yellow) });
     }
 
-    withBlue(blue: ColorTranslator): this & Pick<Ansi, "blue"> {
-        return Object.assign(this, { blue });
+    withBlue(blue: ColorTranslatorInput): this & Pick<Ansi, "blue"> {
+        return Object.assign(this, { blue: new ColorTranslator(...blue) });
     }
 
-    withMagenta(magenta: ColorTranslator): this & Pick<Ansi, "magenta"> {
-        return Object.assign(this, { magenta });
+    withMagenta(magenta: ColorTranslatorInput): this & Pick<Ansi, "magenta"> {
+        return Object.assign(this, { magenta: new ColorTranslator(...magenta) });
     }
 
-    withCyan(cyan: ColorTranslator): this & Pick<Ansi, "cyan"> {
-        return Object.assign(this, { cyan });
+    withCyan(cyan: ColorTranslatorInput): this & Pick<Ansi, "cyan"> {
+        return Object.assign(this, { cyan: new ColorTranslator(...cyan) });
     }
 
-    withWhite(white: ColorTranslator): this & Pick<Ansi, "white"> {
-        return Object.assign(this, { white });
+    withWhite(white: ColorTranslatorInput): this & Pick<Ansi, "white"> {
+        return Object.assign(this, { white: new ColorTranslator(...white) });
     }
 
-    withBrightBlack(brightBlack: ColorTranslator): this & Pick<Ansi, "brightBlack"> {
-        return Object.assign(this, { brightBlack });
+    withBrightBlack(brightBlack: ColorTranslatorInput): this & Pick<Ansi, "brightBlack"> {
+        return Object.assign(this, { brightBlack: new ColorTranslator(...brightBlack) });
     }
 
-    withBrightRed(brightRed: ColorTranslator): this & Pick<Ansi, "brightRed"> {
-        return Object.assign(this, { brightRed });
+    withBrightRed(brightRed: ColorTranslatorInput): this & Pick<Ansi, "brightRed"> {
+        return Object.assign(this, { brightRed: new ColorTranslator(...brightRed) });
     }
 
-    withBrightGreen(brightGreen: ColorTranslator): this & Pick<Ansi, "brightGreen"> {
-        return Object.assign(this, { brightGreen });
+    withBrightGreen(brightGreen: ColorTranslatorInput): this & Pick<Ansi, "brightGreen"> {
+        return Object.assign(this, { brightGreen: new ColorTranslator(...brightGreen) });
     }
 
-    withBrightYellow(brightYellow: ColorTranslator): this & Pick<Ansi, "brightYellow"> {
-        return Object.assign(this, { brightYellow });
+    withBrightYellow(brightYellow: ColorTranslatorInput): this & Pick<Ansi, "brightYellow"> {
+        return Object.assign(this, { brightYellow: new ColorTranslator(...brightYellow) });
     }
 
-    withBrightBlue(brightBlue: ColorTranslator): this & Pick<Ansi, "brightBlue"> {
-        return Object.assign(this, { brightBlue });
+    withBrightBlue(brightBlue: ColorTranslatorInput): this & Pick<Ansi, "brightBlue"> {
+        return Object.assign(this, { brightBlue: new ColorTranslator(...brightBlue) });
     }
 
-    withBrightMagenta(brightMagenta: ColorTranslator): this & Pick<Ansi, "brightMagenta"> {
-        return Object.assign(this, { brightMagenta });
+    withBrightMagenta(brightMagenta: ColorTranslatorInput): this & Pick<Ansi, "brightMagenta"> {
+        return Object.assign(this, { brightMagenta: new ColorTranslator(...brightMagenta) });
     }
 
-    withBrightCyan(brightCyan: ColorTranslator): this & Pick<Ansi, "brightCyan"> {
-        return Object.assign(this, { brightCyan });
+    withBrightCyan(brightCyan: ColorTranslatorInput): this & Pick<Ansi, "brightCyan"> {
+        return Object.assign(this, { brightCyan: new ColorTranslator(...brightCyan) });
     }
 
-    withBrightWhite(brightWhite: ColorTranslator): this & Pick<Ansi, "brightWhite"> {
-        return Object.assign(this, { brightWhite });
+    withBrightWhite(brightWhite: ColorTranslatorInput): this & Pick<Ansi, "brightWhite"> {
+        return Object.assign(this, { brightWhite: new ColorTranslator(...brightWhite) });
     }
 
     build(this: Ansi) {
@@ -151,28 +153,31 @@ class TermTheme {
 
     static fromAlacritty(themeText: string): TermTheme {
         var data = toml.parse(themeText);
-        
+
+        const builder = new AnsiBuilder()
+        const ansi = builder
+            .withBlack(data.colors.normal.black)
+            .withRed(data.colors.normal.red)
+            .withGreen(data.colors.normal.green)
+            .withYellow(data.colors.normal.yellow)
+            .withBlue(data.colors.normal.blue)
+            .withMagenta(data.colors.normal.magenta)
+            .withCyan(data.colors.normal.cyan)
+            .withWhite(data.colors.normal.white)
+            .withBrightBlack(data.colors.bright.black)
+            .withBrightRed(data.colors.bright.red)
+            .withBrightGreen(data.colors.bright.green)
+            .withBrightYellow(data.colors.bright.yellow)
+            .withBrightBlue(data.colors.bright.blue)
+            .withBrightMagenta(data.colors.bright.magenta)
+            .withBrightCyan(data.colors.bright.cyan)
+            .withBrightWhite(data.colors.bright.white)
+            .build();
+
         return new TermTheme(
             new ColorTranslator(data.colors.primary.background),
             new ColorTranslator(data.colors.primary.foreground),
-            {
-                black: new ColorTranslator(data.colors.normal.black),
-                red: new ColorTranslator(data.colors.normal.red),
-                green: new ColorTranslator(data.colors.normal.green),
-                yellow: new ColorTranslator(data.colors.normal.yellow),
-                blue: new ColorTranslator(data.colors.normal.blue),
-                magenta: new ColorTranslator(data.colors.normal.magenta),
-                cyan: new ColorTranslator(data.colors.normal.cyan),
-                white: new ColorTranslator(data.colors.normal.white),
-                brightBlack: new ColorTranslator(data.colors.bright.black),
-                brightRed: new ColorTranslator(data.colors.bright.red),
-                brightGreen: new ColorTranslator(data.colors.bright.green),
-                brightYellow: new ColorTranslator(data.colors.bright.yellow),
-                brightBlue: new ColorTranslator(data.colors.bright.blue),
-                brightMagenta: new ColorTranslator(data.colors.bright.magenta),
-                brightCyan: new ColorTranslator(data.colors.bright.cyan),
-                brightWhite: new ColorTranslator(data.colors.bright.white),
-            }
+            ansi
         )
     }
 }
