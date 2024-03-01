e728c3d18c015e5e761ba8c2e1e2e06ed17eb940
Fisher Sun <f.sun@wustl.edu>
2024-02-29 22:44:28 -0600

    Configure build for library
    https://andrewwalpole.com/blog/use-vite-for-javascript-libraries/
    
    https://vitejs.dev/guide/build#library-mode

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
-
+export * as rgb_color from "./rgb_color";
+export type { RgbColor } from "./rgb_color";
+export * as terminal_theme from "./terminal_theme";
+export type { TerminalTheme } from "./terminal_theme";
